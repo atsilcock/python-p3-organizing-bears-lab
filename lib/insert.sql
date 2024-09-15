0adab45178598c28dc166c268bdbc6cc912e6742
Alin Silcock <at.silcock@gmail.com>
2024-09-14 23:03:23 -0600

    update

---
+++ lib/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO bears ("name", "sex") VALUES ("Mr.Chocalate", "F"), ("Rowdy", "F"), ("Tabitha", "M"), ("Sergeant Brown", "M"),("Melissa", "F"), ("Grinch", "M"), ("Wendy", "F"),  (NULL,"F")
+
